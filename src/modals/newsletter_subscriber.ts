60de46e709a251850872996fed5729a93d2dea82
theo-ack <theo4701@gmail.com>
2025-02-14 11:42:19 +0100

    fixed get newsletter_subscriber

--- src/modals/newsletter_subscriber.ts
+++ src/modals/newsletter_subscriber.ts
@@ -2,7 +2,7 @@ import { getPool } from "../db/db";
 
 export class Newsletter_subscriber {
   static async findAll() {
-    const result = await getPool().query("SELECT * FROM newsletter_subscriber");
+    const result = await getPool().query("SELECT * FROM subscriber_newsletter");
     return result.rows;
   }
 }
